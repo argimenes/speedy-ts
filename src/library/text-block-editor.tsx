7599c31f0efa86d00e75237d0f14a6df66262664
Iian Neill <iian.d.neill@gmail.com>
2024-04-15 16:16:57 +1000

    More work

--- src/library/text-block-editor.tsx
+++ src/library/text-block-editor.tsx
@@ -1,3 +1,5 @@
+import { Property } from "./speedy";
+
 export interface ICellConstructor {
     text: string;
     previous?: Cell;
@@ -7,11 +9,22 @@ export interface IStandoffProperty {
     type: string;
     startIndex: number;
     endIndex: number;
+    value?: Record<string,string>;
+}
+export interface IStandoffPropertySchema {
+    type: string;
+    bindings: string[];
+    decorate: {
+        cellClass?: string;
+        propertyBlockClass?: string;
+        batchRender?: (args: { editor: TextBlockEditor, properties: StandoffProperty[] }) => void;
+    }
 }
 export interface ITextBlock {
-    id: GUID;
+    id?: GUID;
     text: string;
     properties: IStandoffProperty[];
+    schemas: IStandoffPropertySchema[];
 }
 
 export class Cell {
@@ -20,9 +33,11 @@ export class Cell {
     next?: Cell;
     text: string;
     element?: HTMLSpanElement;
+    isLineBreak: boolean;
     constructor({ text }: ICellConstructor) {
         this.index = 0;
         this.text = text;
+        this.isLineBreak = false;
     }
     renderNode() {
         const span = document.createElement("SPAN");
@@ -36,18 +51,48 @@ export class Cell {
     removeNode() {
         this.element?.remove();
     }
+    getTextNode() {
+        // Get the first TEXT NODE of the element
+        let node = this.element?.firstChild;
+        if (!node) {
+            return this.element as ChildNode;
+        }
+        while (node?.nodeType != 3) {
+            node = node?.firstChild as ChildNode;
+        }
+        return node;
+    }
+}
+
+export enum Caret {
+    Left = 0,
+    Right = 1
 }
 
 export type GUID = string;
+
+export class StandoffProperty {
+    id?: GUID;
+    start: Cell;
+    end: Cell;
+    isDeleted: boolean;
+    constructor({ start, end }: { start: Cell, end: Cell }) {
+        this.isDeleted = false;
+        this.start = start;
+        this.end = end;
+    }
+}
 
 export class TextBlockEditor {
     id: GUID;
     container: HTMLDivElement;
     cells: Cell[];
+    properties: StandoffProperty[];
     constructor({ container }: { container: HTMLDivElement }) {
         this.id = "";
         this.cells = [];
         this.container = container;
+        this.properties = [];
     }
     unbind() {
         const block = {} as ITextBlock;
@@ -106,7 +151,160 @@ export class TextBlockEditor {
         cell.next = next;
         next.previous = cell;
     }
-    removeCharacterAtIndex(index: number) {
-
+    removeCell(args: { cell: Cell, updateCaret?: boolean }) {
+        const { cell} = args;
+        const updateCaret = !!args.updateCaret;
+        const previous = cell.previous;
+        const next = cell.next;
+        if (cell.isLineBreak) {
+            /**
+             * Emit an event indicating this.
+             */
+            return;
+        }
+        if (previous) {
+            previous.next = next;
+        }
+        if (next) {
+            next.previous = previous;
+        }
+        cell.removeNode();
+        if (updateCaret) {
+            // if (next) {
+            //     this.setCarotByNode({ node: next, offset: Caret.Left });
+            // }
+            // this.mark();
+            // this.updateOffsets();
+            // if (this.onTextChanged) {
+            //     const caret = {
+            //         left: previous, right: next, container: currentTextBlock
+            //     };
+            //     const { text, cells } = this.getTextBlockData(currentTextBlock);
+            //     this.onTextChanged({
+            //         action: "deleted", editor: this, caret, text, cells
+            //     });
+            // }
+        }
+    }
+    setCarotByNode(args: { node: Cell, offset?: Caret }) {
+        /**
+         * Might want to investigate setting the caret by absolutely positioning an SVG ...
+         */
+        const { node } = args;
+        if (!node) {
+            return;
+        }
+        const selection = document.getSelection() as globalThis.Selection;
+        const range = document.createRange();
+        const offset = (args.offset != null) ? args.offset : Caret.Right;
+        const textNode = node.getTextNode();
+        range.setStart(textNode, 1);
+        range.collapse(true);
+        if (selection.setBaseAndExtent) {
+            var startOffset = offset;    // range.startOffset;
+            var endOffset = offset;      // range.endOffset;
+            selection.setBaseAndExtent(range.startContainer, startOffset, range.endContainer, endOffset);
+        } else {
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
+    }
+    shiftPropertyBoundaries(cell: Cell) {
+        this.shiftPropertyStartNodesRight(cell);
+        this.shiftPropertyEndNodesLeft(cell);
+    }
+    shiftPropertyEndNodesLeft(cell: Cell) {
+        const previousCell = cell.previous;
+        const properties = this.properties.filter(p => !p.isDeleted);
+        const singles = properties.filter(p => p.start == p.end && p.start == cell);
+        if (singles) {
+            singles.forEach(p => p.isDeleted = true);
+        }
+        const endProperties = properties.filter(p => p.end == cell && p.start != cell);
+        if (endProperties.length) {
+            if (!previousCell) {
+                endProperties.forEach(p => p.isDeleted = true);
+            } else {
+                endProperties.forEach(p => p.end = previousCell);
+            }
+        }
+    }
+    shiftPropertyStartNodesRight(cell: Cell) {
+        const nextCell = cell.next;
+        const properties = this.properties.filter(p => !p.isDeleted);
+        const singles = properties.filter(p => p.start == p.end && p.start == cell);
+        if (singles.length) {
+            singles.forEach(p => p.isDeleted = true);
+        }
+        const startProperties = properties.filter(p => p.start == cell && p.end != cell);
+        if (startProperties.length) {
+            if (!nextCell) {
+                startProperties.forEach(p => p.isDeleted = true);
+            } else {
+                startProperties.forEach(p => p.start = nextCell);
+            }
+        }
+    }
+    removeCellAtIndex({ index, updateCaret }: { index: number, updateCaret?: boolean }) {
+        const cell = this.cells.find(c => c.index == index);
+        if (!cell) {
+            log("Cell could not be found for the index", { index, error: new Error() });
+            return;
+        }
+        return this.removeCell({ cell, updateCaret });
     }
 }
+
+const log = (msg: string, data: Record<string,any>) => console.log(msg, data);
+
+
+const editor = new TextBlockEditor({
+    container: document.createElement("DIV") as HTMLDivElement
+});
+editor.bind({
+    text: "Once upon a midnight dreary ...",
+    properties: [
+        {
+            type: "style/italics",
+            startIndex: 5,
+            endIndex: 9
+        },
+        {
+            type: "codex/entity-reference",
+            startIndex: 13,
+            endIndex: 21
+        },
+        {
+            type: "style/blur",
+            startIndex: 23,
+            endIndex: 29
+        },
+    ],
+    schemas: [
+        {
+            type: "style/italics",
+            bindings: ["control-i"],
+            decorate: {
+                cellClass: "italics"
+            }
+        },
+        {
+            type: "codex/entity-reference",
+            bindings: ["control-e", "double click"],
+            decorate: {
+                batchRender: (args) => {
+                    const { editor, properties } = args;
+                    const { container } = editor;
+                    // Draw purple SVG underlines for entities, etc.
+                }
+            }
+        },
+        {
+            type: "style/blur",
+            bindings: ["control-b"],
+            decorate: {
+                propertyBlockClass: "blur"
+            }
+        },
+    ]
+})
