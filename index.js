5a7e8cfd3bab62e734510b3c21ef3a30c917cd10
Iian Neill <iian.d.neill@gmail.com>
2024-05-16 15:37:11 +1000

    Node

---
+++ index.js
@@ -0,0 +1,15 @@
+// Import essential libraries 
+const express = require('express'); 
+const app = express(); 
+const path = require('path'); 
+const router = express.Router(); 
+// Setup essential routes 
+router.get('/', function(req, res) { 
+     res.sendFile(path.join(__dirname + '/index.html')); 
+    //__dirname : It will resolve to your project folder. 
+}); 
+//add the router
+
+app.use('/', router); 
+app.listen(process.env.port || 3000); 
+console.log('Running at Port 3000'); 
