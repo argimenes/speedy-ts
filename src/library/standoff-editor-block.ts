0d4fd86b440d86aa32d3f74b9ff6ad2751cf560c
Iian <iian.d.neill@gmail.com>
2024-07-02 22:40:00 +1000

    Refactoring

--- src/library/standoff-editor-block.ts
+++ src/library/standoff-editor-block.ts
@@ -1,11 +1,10 @@
 import _ from "underscore";
 import { KEYS, Platform, TPlatformKey } from "./keyboard";
-import { v4 as uuidv4 } from 'uuid';
-import { updateElement, wrapRange } from "./svg";
+import { updateElement } from "./svg";
 import { AbstractBlock, BlockPropertyDto, Commit, GUID, IAbstractBlockConstructor, IBlockDto, IBlockPropertySchema, IKeyboardInput, InputAction, InputEventSource, ModeTrigger, InputEvent, BlockType,IBindingHandlerArgs, IBlockRelation} from "./abstract-block";
 import { Caret, Cell, CellHtmlElement, CellNode, ICoordOffsets, Row } from "./cell";
-import { IPlugin } from "./plugins/clock";
 import { BlockProperty } from "./block-property";
+import { StandoffProperty } from "./standoff-property";
 
 export enum CARET {
     LEFT = 0,
@@ -115,143 +114,7 @@ function groupBy<T extends object> (list: T[], keyGetter: (item: T) => any){
 };
 
 
-export class StandoffProperty {
-    id: GUID;
-    type: string;
-    start: Cell;
-    end: Cell;
-    isDeleted: boolean;
-    cache: Record<string, any>;
-    value: string;
-    metadata: Record<string, any>;
-    plugin?: IPlugin;
-    schema: IStandoffPropertySchema;
-    block: StandoffEditorBlock; 
-    bracket: { left?: HTMLElement; right?: HTMLElement };
-    constructor({ type, start, end, block, id, schema }: IStandoffPropertyConstructor) {
-        this.id = id || uuidv4();
-        this.isDeleted = false;
-        this.type = type;
-        this.start = start;
-        this.end = end;
-        this.schema = schema;
-        this.metadata = {};
-        this.value = "";
-        this.block = block;
-        this.cache = {};
-        this.bracket = {
-            left: undefined,
-            right: undefined
-        };
-        this.onInit();
-    }
-    onInit() {
-        if (this.schema?.event?.onInit) {
-            this.schema?.event?.onInit(this);
-        }
-    }
-    onDestroy() {
-        if (this.schema?.event?.onDestroy) {
-            this.schema?.event?.onDestroy(this);
-        }
-    }
-    destroy() {
-        this.isDeleted = true;
-        this.onDestroy();
-        if (this.schema.decorate?.cssClass) {
-            this.detachCssClass(this.schema.decorate?.cssClass);
-        }
-    }
-    hasOffsetChanged() {
-        let spoff = this.start.cache.previousOffset;
-        let soff = this.start.cache.offset;
-        if (!soff || !spoff) {
-            return true;
-        }
-        let startSame = spoff.x == soff.x && spoff.y == soff.y;
-        if (!startSame) {
-            return true;
-        }
-        let epoff = this.end.cache.previousOffset;
-        let eoff = this.end.cache.offset;
-        if (!epoff || !eoff) {
-            return true;
-        }
-        let endSame = epoff.x == eoff.x && epoff.y == eoff.y;
-        if (!endSame) {
-            return true;
-        }
-        return false;
-    }
-    scrollTo() {
-        this.start.element?.scrollIntoView();
-    }
-    applyStyling() {
-        if (this.schema?.decorate?.cssClass) {
-            this.applyCssClass(this.schema.decorate.cssClass);
-        }
-        if (this.schema?.wrap?.cssClass) {
-            const wrapper = wrapRange(this);
-            wrapper.classList.add(this.schema?.wrap?.cssClass);
-        }
-    }
-    applyCssClass(className: string) {
-        if (this.isDeleted) return;
-        const cells = this.getCells();
-        cells.forEach(x => x.element?.classList.add(className));
-    }
-    detachCssClass(className: string) {
-        const cells = this.getCells();
-        cells.forEach(x => x.element?.classList.remove(className));
-    }
-    removeCssClassFromRange(className: string) {
-        if (this.isDeleted) return;
-        const cells = this.getCells();
-        cells.forEach(x => x.element?.classList.remove(className));
-    }
-    getCells() {
-        const cells: Cell[] = [];
-        let cell = this.start;
-        let looping = true;
-        if (this.start == this.end) {
-            cells.push(this.start);
-            return cells;
-        }
-        while (looping && !!cell) {
-            cells.push(cell);
-            let next = cell.next;
-            if (!next) {
-                looping = false;
-            }
-            if (next == this.end) {
-                cells.push(next);
-                looping = false;
-            }
-            cell = next as Cell;
-        }
-        return cells;
-    }
-    serialize() {
-        return {
-            id: this.id,
-            type: this.type,
-            start: this.start.index,
-            end: this.end.index,
-            value: this.value,
-            metadata: this.metadata,
-            plugin: this.plugin ? this.plugin.serialise() : null,
-            isDeleted: this.isDeleted
-        }
-    }
-    showBrackets() {
-        if (this.bracket.left) this.bracket.left.style.display = "inline";
-        if (this.bracket.right) this.bracket.right.style.display = "inline";
-    }
-    hideBrackets() {
-        if (this.bracket.left) this.bracket.left.style.display = "none";
-        if (this.bracket.right) this.bracket.right.style.display = "none";
-    }
-}
+
 export interface IStandoffEditorBlockDto extends IBlockDto {
     text: string;
     standoffProperties?: StandoffPropertyDto[];
