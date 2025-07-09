84be744166df81d3312bff32e98bcfaa245658b5
Iian Neill <iian.d.neill@gmail.com>
2025-07-09 10:29:10 +1000

    Now loading SurrealDB local node if not already launched

--- publish.ps1
+++ publish.ps1
@@ -6,6 +6,18 @@
 # }
 # Get-ChildItem -Path $Source\*.* -Recurse | Remove-Item -Force
 # Get-ChildItem -Path $Target\*.* -Recurse | Remove-Item -Force
+
+# Check if SurrealDB is already running on the specified port
+$connection = Test-NetConnection -ComputerName "localhost" -Port 8000
+
+if ($connection.TcpTestSucceeded) {
+    Write-Host "SurrealDB is already running on port 8000."
+} else {
+    Write-Host "SurrealDB is not running on port $port. Starting the server..."
+    # Run the SurrealDB command to start the server
+    Start-Process "surreal" -ArgumentList "start", "rocksdb://./surrealDB", "-u", "root", "-p", "root"
+}
+
 npm run build
 # Copy-Item -Path $Source\* -Destination $Target -Recurse -Force
 node ./dist/server/index.js
